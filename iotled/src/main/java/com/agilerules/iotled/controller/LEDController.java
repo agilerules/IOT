4dd06e766425be89ec23cb3ba4fcd638f83f0caa
agilerules <srini@agilerulesconsultants.com>
2016-05-11 13:52:27 +0530

    First version

---
+++ iotled/src/main/java/com/agilerules/iotled/controller/LEDController.java
@@ -0,0 +1,30 @@
+package com.agilerules.iotled.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class LEDController {
+	
+	public static GpioPinDigitalOutput pin;
+	
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello World!!";
+	}
+	
+	@RequestMapping("/light")
+	public String light(){
+		if(pin==null){
+			GpioController gpio = GpioFactory.getInstance();
+			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01,"My LED",PinState.LOW);
+		}
+		pin.toggle();
+		return "Response from light!!";
+	}
+
+}
